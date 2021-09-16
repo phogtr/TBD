7165d805eb19e5e3a4fea5fe96e31eb5508ea1da
phogtr <phogbtr@gmail.com>
2021-09-15 19:19:38 -0700

    updated username column of users table

--- server/src/model/database.sql
+++ server/src/model/database.sql
@@ -1,17 +1,19 @@
 CREATE DATABASE PostgresNextSass;
 
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users(
   user_id uuid DEFAULT uuid_generate_v4(),
-  user_name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL,
   PRIMARY KEY(user_id)
 );
 
 
-INSERT INTO users (user_name, user_email, user_password)
+INSERT INTO users (username, user_email, user_password)
 VALUES('test', 'a@a.a', 'abc123');
 
 select * from users;
 
-delete from users where user_name = 'test';
+delete from users where username = 'test';
