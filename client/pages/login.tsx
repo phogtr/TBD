8622c1c2e25e4a4f235bf83124b7541105e6946c
phogtr <phogbtr@gmail.com>
2021-09-18 15:46:58 -0700

    login form, cors credentials for cookies

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,13 +1,66 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Meta } from "../components/Meta";
+import { server } from "../config";
+
+interface ILoginBody {
+  email: string;
+  password: string;
+}
 
 interface IloginProps {}
 
 const Login: React.FC<IloginProps> = ({}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div>
       <Meta title="Login" />
       <h1>Login</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const loginBody: ILoginBody = {
+            email,
+            password,
+          };
+          try {
+            const res = await axios.post(`${server}/api/login`, loginBody, {
+              withCredentials: true,
+            });
+            console.log(res.data);
+          } catch (error) {
+            console.log(error);
+          }
+        }}
+      >
+        <div>
+          <label>
+            Email:
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
