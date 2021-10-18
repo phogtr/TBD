e549ac741a42f86f7af845a771e470bdc88a88b5
phogtr <phogbtr@gmail.com>
2021-10-17 17:55:49 -0700

    basic tickets & locations routes

---
+++ client/pages/locations/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ILocationsProps {}
+
+const Locations: React.FC<ILocationsProps> = ({}) => {
+  return (
+    <div>
+      <h1>Locations</h1>
+    </div>
+  );
+};
+export default Locations;
---
+++ client/pages/tickets/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ITicketsProps {}
+
+const Tickets: React.FC<ITicketsProps> = ({}) => {
+  return (
+    <div>
+      <h1>Tickets</h1>
+    </div>
+  );
+};
+export default Tickets;
