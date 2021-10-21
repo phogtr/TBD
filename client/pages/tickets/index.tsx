c0e42acd6a9848fb61a843f481b223b7d8aa9639
phogtr <phogbtr@gmail.com>
2021-10-20 20:59:26 -0700

    SSR data for Tickets

--- client/pages/tickets/index.tsx
+++ client/pages/tickets/index.tsx
@@ -1,12 +1,41 @@
+import { GetServerSideProps } from "next";
 import React from "react";
+import { getAllTicketsRequest } from "../../api/ticket/ticket.api";
 
-interface ITicketsProps {}
+interface IDestination {
+  id: string;
+  name: string;
+}
 
-const Tickets: React.FC<ITicketsProps> = ({}) => {
+interface ITicketsData {
+  id: string;
+  destination: IDestination;
+}
+
+interface ITicketsProps {
+  tickets: ITicketsData[];
+}
+
+const Tickets: React.FC<ITicketsProps> = ({ tickets }) => {
   return (
     <div>
       <h1>Tickets</h1>
+      <div>
+        {tickets.map((t) => (
+          <div key={t.id}>Location: {t.destination.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
 export default Tickets;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await getAllTicketsRequest();
+
+  return {
+    props: {
+      tickets: res.data,
+    },
+  };
+};
