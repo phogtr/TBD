7891bd87c76ed9529e3dc205b4f1569eaf489341
phogtr <phogbtr@gmail.com>
2021-10-22 18:48:14 -0700

    init create-ticket page

---
+++ client/pages/tickets/create-ticket.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ICreateTicketProps {}
+
+const CreateTicket: React.FC<ICreateTicketProps> = ({}) => {
+  return (
+    <div>
+      <h1>Create a Ticket</h1>
+    </div>
+  );
+};
+export default CreateTicket;
