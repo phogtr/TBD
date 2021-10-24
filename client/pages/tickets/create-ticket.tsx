17df8557227e4d28adf7b8fa057dfa0ba0f91185
phogtr <phogbtr@gmail.com>
2021-10-23 19:48:34 -0700

    more init create-ticket page

--- client/pages/tickets/create-ticket.tsx
+++ client/pages/tickets/create-ticket.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Meta } from "../../components/Meta";
 
 interface ICreateTicketProps {}
 
 const CreateTicket: React.FC<ICreateTicketProps> = ({}) => {
+  const [selectedItem, setSelectedItem] = useState("");
+
   return (
     <div>
+      <Meta title="Create Ticket" />
       <h1>Create a Ticket</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(selectedItem);
+        }}
+      >
+        <div>
+          <label>
+            Select a location:
+            <select value={selectedItem} onChange={(e) => setSelectedItem(e.target.value)}>
+              <option value="item 1">item 1</option>
+              <option value="item 2">item 2</option>
+              <option value="item 3">item 3</option>
+            </select>
+          </label>
+        </div>
+        <button type="submit" value="submit">
+          Create
+        </button>
+      </form>
     </div>
   );
 };
