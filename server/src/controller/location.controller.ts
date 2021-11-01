60700a6e32cbe7bb88c78f06e3ac4ff365e0e66a
phogtr <phogbtr@gmail.com>
2021-10-31 20:23:48 -0700

    get available locations api

--- server/src/controller/location.controller.ts
+++ server/src/controller/location.controller.ts
@@ -36,3 +36,30 @@ export const getAllLocationsHandler = async (_req: Request, res: Response) => {
     return res.sendStatus(500);
   }
 };
+
+export const getAvailableLocationsHandler = async (_req: Request, res: Response) => {
+  try {
+    // const locationCount = await prisma.location.count({
+    //   where: {
+    //     ticket: {
+    //       is: null,
+    //     },
+    //   },
+    // });
+    const validLocations = await prisma.location.findMany({
+      where: {
+        ticket: {
+          is: null,
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+    return res.json(validLocations);
+  } catch (error) {
+    console.log("error: ", error);
+    return res.sendStatus(500);
+  }
+};
--- server/src/routes/location.route.ts
+++ server/src/routes/location.route.ts
@@ -1,8 +1,14 @@
 import { Express } from "express";
-import { createLocationHandler, getAllLocationsHandler } from "../controller/location.controller";
+import {
+  createLocationHandler,
+  getAllLocationsHandler,
+  getAvailableLocationsHandler,
+} from "../controller/location.controller";
 
 export default function (app: Express) {
   app.post("/api/create-location", createLocationHandler);
 
   app.get("/api/all-locations", getAllLocationsHandler);
+
+  app.get("/api/available-locations", getAvailableLocationsHandler);
 }
