0ce232df268aa501db106bba2960cdd9986b662e
phogtr <phogbtr@gmail.com>
2021-11-05 18:36:09 -0700

    available des api for destinations page

--- client/pages/destinations/index.tsx
+++ client/pages/destinations/index.tsx
@@ -1,13 +1,32 @@
 import { GetServerSideProps } from "next";
 import React from "react";
+import { getAvailableDestinationRequest } from "../../api/destination/destination.api";
+import { IDestination } from "../../interface";
 
-interface IDestinationsProps {}
+interface IDestinationsProps {
+  destinations: IDestination[];
+}
 
-const Destinations: React.FC<IDestinationsProps> = ({}) => {
+const Destinations: React.FC<IDestinationsProps> = ({ destinations }) => {
   return (
     <div>
       <h1>Destinations</h1>
+      <div>
+        {destinations.map((d) => (
+          <div key={d.id}>{d.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
 export default Destinations;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const res = await getAvailableDestinationRequest();
+
+  return {
+    props: {
+      destinations: res.data,
+    },
+  };
+};
